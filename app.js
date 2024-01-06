868f101ac26dc7bc8d29de61e06425710e58b375
Teufran Kunst <meucanal491@gmail.com>
2024-01-06 17:35:46 -0300

    Projeto Inicial

---
+++ app.js
@@ -0,0 +1,54 @@
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
+let tentativas = 1;
+
+exibirMensagemInicial()
+
+function exibirTextoNaTela (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female',
+    {rate: 1});
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto!');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 10:');
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+           exibirTextoNaTela('p', `O número secreto é menor do que ${chute}!`) ;
+        } else {
+            exibirTextoNaTela('p', `O número secreto é maior do que ${chute}!`);
+        }
+        tentativas ++;
+        limparCampo(); 
+    }
+}
+
+function botaoReiniciar() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
